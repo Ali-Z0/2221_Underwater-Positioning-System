cfa31a6366ce03ee281c2579f441977d1d3f6961
Ali-Z0 <ali.zoubir@bluewin.ch>
2023-04-05 16:56:55 +0200

    Update all

---
+++ soft/Test_Pinning/firmware/Test_Pinning.X/build/default/production/_ext/1360937237/main.o.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/1360937237/main.o: ../src/main.c \
+ ../../../../framework/system/common/sys_module.h \
+ ../../../../framework/system/common/sys_common.h
+
+../../../../framework/system/common/sys_module.h:
+
+../../../../framework/system/common/sys_common.h:
